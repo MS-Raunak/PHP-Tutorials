c252fd867ea98695733811efa8ab3e6c361072b8
MS-Raunak <raunakkr.saha@gmail.com>
2024-05-31 02:00:54 +0530

    variable

--- index.php
+++ index.php
@@ -7,16 +7,42 @@
 </head>
 <body>
     <h1>This is my first php code</h1>
-    /**
-     echo is used to print value
-     */
+   
+    <!-- 
+        - Php is not a case sensitive language
+        - echo is used to print value
+     -->
+     
     <?php  echo"hello world!!";
     ?>
-
     <h1>
         <?php 
             echo"Hello I am PHP" 
         ?>
     </h1>
+
+    
+        <!-- 
+            Variable: 
+            Syntax $Variable varName;
+            php is dynamically typed programming language.
+            putting semicolon is mandatory after completing the statement
+        -->
+
+    <?php
+
+    $Variable1 = 10;
+    $Variable2 = 20;
+
+    $a = 30;
+
+    $sum = $Variable1 + $Variable2 + $a;
+
+    echo $Variable1;
+    echo $Variable2;
+    echo $a;
+
+    echo "Sum of $Variable1 + $Variable2 + $a =  $sum";  //Concatination
+    ?>
 </body>
 </html>
