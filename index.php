76213c519838c40b8e498a9351826ae1b2496185
MS-Raunak <raunakkr.saha@gmail.com>
2024-05-31 01:40:39 +0530

    print statement

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php Tutorials</title>
+</head>
+<body>
+    <h1>This is my first php code</h1>
+    /**
+     echo is used to print value
+     */
+    <?php  echo"hello world!!";
+    ?>
+
+    <h1>
+        <?php 
+            echo"Hello I am PHP" 
+        ?>
+    </h1>
+</body>
+</html>
