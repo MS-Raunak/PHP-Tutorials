35003e674059f122817d0d8c808ea9f7b68a2b0e
MS-Raunak <raunakkr.saha@gmail.com>
2024-06-12 16:13:09 +0530

    git tutorial: data types

--- ex.php
+++ ex.php
@@ -1,77 +1,51 @@
 <?php
-//*********OPERATORS********//
-    //Arithmetic Operator
-    echo "Arithmetic<br>==============<br>";
-    $a = 10; $b=10;
-    echo "Sum:".$a+$b . "<br><br>";
-//###############################################################
+//*********DATA TYPES********//
+    //1.Integer
+    echo "Integer<br>==============<br>";
+    echo "10: ";
+    echo var_dump(10); //int(10)
 
-    //Relational Operator(Comparison)
-    echo "Relational(Comparison)<br>=================<br>";
+    //2.Float
+    echo "<br><br>Float<br>==============<br>";
+    echo "10.2: ";
+    echo var_dump(10.2); //float(10)
 
-    echo "10<20: ".(10<20) ."<br>"; //1
-    echo "10<20: ".(10>20). "<br>"; //blank
-    echo "10<20: ".var_dump(10<20). "<br>";  //bool(true)
-    echo "10<20: ".var_dump(10==20). "<br>";  //bool(flase)
-    echo "10<20: ".var_dump(10<=20). "<br><br>";  //bool(true)
-//################################################################
+    //3.String
+    echo "<br><br>String<br>==============<br>";
+    echo "hello: ";
+    echo var_dump("hello"); //string(5) "hello"
 
-    //Logical Operator//
-    echo "Logical<br>==============<br>"; 
-    echo "Logical(AND)<br>==============<br>"; 
-    //AND operator(and/AND/&&)
-    echo "6>1 and 6<10:  ";
-    echo var_dump(6>1 and 6<10); //bool(true)
+    //4.Boolean
+    echo "<br><br>String<br>==============<br>";
+    echo "TRUE: ";
+    echo var_dump(true); //bool(true)
 
-    echo "<br> 1>6 and 1<10:  ";
-    echo var_dump(1>6 and 1<10); //bool(flase)
-
-    //OR operator(or/||)
-    echo "<br><br>Logical(OR)<br>=============="; 
-    echo "<br> 1>6 OR 1<10:  ";
-    echo var_dump(1>6 or 1<10); //bool(true)
-
-    //NOT operator(or/!)
-    echo "<br><br>Logical(NOT)<br>=============="; 
-    echo "<br> !(1>6):  ";
-    echo var_dump(!(1>6)); //bool(true)
-
-    //Xor operator: if both operands are different the return true otherwise false
-    echo "<br><br>XOR<br>==============<br>"; 
-    echo "true xor true:";
-    echo var_dump(true xor true); //bool(flase)
-    echo "<br>true xor false:";
-    echo var_dump(true xor false); //bool(true)
-//################################################################
-
-    //Increment Opertaor//
-    echo "<br><br>INCREMENT<br>==============<br>"; 
-
-    //Pre Increment/Decrement Opertaor
-    echo "Increment/Decrement(Pre)<br>==============<br>"; 
-    $m = 10; 
-    echo "m: 10";
-    echo "<br>++m: ". ++$m; //11
-    echo "<br>--m: ". --$m; //10
-    
-
-     //POST Increment/Decrement Opertaor
-     echo "<br>Increment/Decrement(Post)<br>==============<br>"; 
-     $k = 10; 
-     echo "k:10"; 
-     echo "<br>k++: ". $k++; //10
-     echo "<br>k: ". $k; //11
-     echo "<br>k--: ". $k--; //11
-     echo "<br>k: ". $k; //10
-//#################################################################
-     
-
-     //Assignment opertor
-     echo "<br><br>Assignment<br>==============<br>"; 
-     echo "10==10: ";
-     echo var_dump(10==10);
-
-     
+    //5.Array
+    echo "<br><br>Array<br>==============<br>";
+    $arr = array(10,20,22.7, "coco");
+    echo "arr = array(10,20, 'coco', 22.7): ";
+    echo var_dump($arr); //array(4) { [0]=> int(10) [1]=> int(20) [2]=> float(22.7) [3]=> string(4) "coco" }
+   
+    //6.Object
+    //7.Callable
+    //8.Iterable
+    //9.Resource
+    //10.Null
 
 
+    /**
+     * Predefined Datatypes
+     * 1.Integer
+     * 2.Boolean
+     * 3.Float
+     * 4.String
+     * 
+     * User-defined Datatypes
+     * 1.Arrray
+     * 2.Objects
+     * 
+     * Special Datatypes
+     * 1.NULL
+     * 2.Resource
+     */
 ?>
