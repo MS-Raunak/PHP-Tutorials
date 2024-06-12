c36d07e487f4c6dbacd6a50591159d1b3c23d03e
MS-Raunak <raunakkr.saha@gmail.com>
2024-06-12 18:04:10 +0530

    git tutorial: conditional statements

--- ex.php
+++ ex.php
@@ -1,51 +1,79 @@
 <?php
-//*********DATA TYPES********//
-    //1.Integer
-    echo "Integer<br>==============<br>";
-    echo "10: ";
-    echo var_dump(10); //int(10)
+//*********CONDITIONAL STATEMENTS ********//
+    //1.If-Else Statement
+    echo "If-Else Statement<br>==============<br>";
 
-    //2.Float
-    echo "<br><br>Float<br>==============<br>";
-    echo "10.2: ";
-    echo var_dump(10.2); //float(10)
+    $age = 18;
+    echo "Your Age: ". $age. "<br>";
 
-    //3.String
-    echo "<br><br>String<br>==============<br>";
-    echo "hello: ";
-    echo var_dump("hello"); //string(5) "hello"
+    if($age<10){
+        echo "You are Minor!!<br>";
+    }
+    else{
+        echo "You are not Minor<br>";
+    }
 
-    //4.Boolean
-    echo "<br><br>String<br>==============<br>";
-    echo "TRUE: ";
-    echo var_dump(true); //bool(true)
+    //2.If-Else If-Else Statement
+    echo "<br>If-Else If-Else Statement<br>==============<br>";
 
-    //5.Array
-    echo "<br><br>Array<br>==============<br>";
-    $arr = array(10,20,22.7, "coco");
-    echo "arr = array(10,20, 'coco', 22.7): ";
-    echo var_dump($arr); //array(4) { [0]=> int(10) [1]=> int(20) [2]=> float(22.7) [3]=> string(4) "coco" }
-   
-    //6.Object
-    //7.Callable
-    //8.Iterable
-    //9.Resource
-    //10.Null
+    $age = 3;
+    echo "Your Age: ". $age. "<br>";
 
+    if($age<=5){
+        echo "You are Child!!<br>";
+    }
+    else if($age< 18 && $age >5){
+        echo "You are Minor<br>";
+    }
+    else{
+        echo "You are not Minor<br>";
+    }
 
-    /**
-     * Predefined Datatypes
-     * 1.Integer
-     * 2.Boolean
-     * 3.Float
-     * 4.String
-     * 
-     * User-defined Datatypes
-     * 1.Arrray
-     * 2.Objects
-     * 
-     * Special Datatypes
-     * 1.NULL
-     * 2.Resource
-     */
+    //3.Nested If-Else or Ladder If Else
+    echo "<br>Nssted If-Else Statement<br>==============<br>";
+
+    $age = 50;
+    echo "Your Age: ". $age. "<br>";
+
+    if($age<18){
+        echo "You are Minor!!<br>";
+    }
+    else{
+        if($age >= 18 && $age <=40)
+            echo "You are Eligible For Marriage!!<br>";
+        else
+            echo "You are Overaged!!<br>";
+    }
+
+    //Switch-Case Statement
+    //2.If-Else If-Else Statement
+    echo "<br>Switch-Case Statement<br>=================<br>";
+    echo "****WELCOME TO QUIZ GAME*****<br>";
+    echo "Which City OF India is Known As Silicon Valley Of India?<br>";
+    echo "1.Hydrabad<br>2.Noida<br>3.Bengaluru<br>4.Pune<br>";
+
+    $choice = 4;
+    echo "<br>Your Choice: " . $choice."<br>";
+
+    switch ($choice) {
+        case 1:
+            echo "Wrong Answer<br>";
+            break;
+
+        case 2:
+            echo "Wrong Answer<br>";
+            break;
+
+        case 3:
+            echo "Correct Answer!! Bengaluru";
+            break;
+
+        case 4:
+            echo "Wrong Answer<br>";
+            break;
+        
+        default:
+            echo "Invalid Input!!";
+            break;
+    }
 ?>
